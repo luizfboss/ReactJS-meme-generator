b4e691ab6d3f57b615bf4952d296b01a619e9716
luizfboss <88173271+luizfboss@users.noreply.github.com>
2022-08-04 00:28:45 -0300

    update

--- files/src/Main.js
+++ files/src/Main.js
@@ -2,13 +2,13 @@ import React from "react";
 import memesData from "./memesData";
 
 function Main(){
+    const [memeImage, setMemeImage] = React.useState("")
+
+
     function getMemeImage(){
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        let url = memesArray[randomNumber].url
-
-
-
+        setMemeImage(memesArray[randomNumber].url)
     }
 
     return (
@@ -29,6 +29,10 @@ function Main(){
             <div className="meme-button-container">
                 <button onClick={getMemeImage}>Get a New Meme Image</button>
             </div>
+        </div>
+
+        <div className="image-container">
+            <img src={memeImage} className="meme-image" />
         </div>
 
       </section>
