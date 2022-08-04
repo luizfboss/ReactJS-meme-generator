572c568e77b6d1992fe018c056fed15b7d76c828
luizfboss <88173271+luizfboss@users.noreply.github.com>
2022-08-04 19:53:33 -0300

    Update Main.js

--- files/src/Main.js
+++ files/src/Main.js
@@ -2,13 +2,22 @@ import React from "react";
 import memesData from "./memesData";
 
 function Main(){
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
 
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
     function getMemeImage(){
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
 
     return (
@@ -32,7 +41,7 @@ function Main(){
         </div>
 
         <div className="image-container">
-            <img src={memeImage} className="meme-image" />
+            <img src={meme.randomImage} className="meme-image" />
         </div>
 
       </section>
